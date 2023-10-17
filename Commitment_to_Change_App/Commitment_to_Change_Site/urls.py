f259cce1857055a972c7a9518b3ba16b1a09bdcc
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-10-17 15:15:54 -0400

    Fix rename that broke everything

--- Commitment_to_Change_App/Commitment_to_Change_Site/urls.py
+++ Commitment_to_Change_App/Commitment_to_Change_Site/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Commitment_to_Change_App project.
+URL configuration for Commitment_to_Change_Site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('commitmenttochange/', include("commitments.urls")),
     path('admin/', admin.site.urls),
 ]
