68ebabe157bd59648ecdc66f5ff89134a8967b73
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-10-23 13:16:58 -0400

    Add demo of commitment creation and listing

--- Commitment_to_Change_App/commitments/urls.py
+++ Commitment_to_Change_App/commitments/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("template_demo", views.template_demo, name="template demo"),
     path("model_details", views.test_models_show_details, name="model details"),
     path("model_change_title", views.test_models_change_title, name="change title"),
-    path("model_change_descrption", views.test_models_change_description, name="change description"),
+    path("model_change_description", views.test_models_change_description, name="change description"),
+    path("model_list_all", views.test_models_list_commitments, name="list all commitments"),
+    path("model_create", views.test_models_create_commitment, name="create commitment"),
 ]
 
--- Commitment_to_Change_App/commitments/views.py
+++ Commitment_to_Change_App/commitments/views.py
@@ -32,3 +32,20 @@ def test_models_change_description(request):
     print(x.description)
     x.save()
     return HttpResponse("changed description")
+
+def test_models_list_commitments(request):
+    commitments = Commitment.objects.all()
+    responseTable = "<table>"
+    responseTable += "<tr><th>ID</th><th>Title</th><th>Description</th></tr>"
+    for commitment in commitments:
+        responseTable += "<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(commitment.id, commitment.title, commitment.description)
+    responseTable += "</table>"
+    return HttpResponse(responseTable)
+
+def test_models_create_commitment(request):
+    # POST is the same, just replace GET. GET is better for experimenting 
+    # manually but we should use POST when we actually implement stuff.
+    t = request.GET.get("title", "defaultTitle")
+    d = request.GET.get("description", "defaultDescription")
+    x = Commitment.objects.create(title=t, description=d)
+    return HttpResponse("Commitment {} created".format(x.id))
