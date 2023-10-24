3e88c64ea30a8af5b67885b46f770c840718bea4
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-10-24 11:50:03 -0400

    Make created and updated fields handle themselves

--- Commitment_to_Change_App/commitments/models.py
+++ Commitment_to_Change_App/commitments/models.py
@@ -17,8 +17,8 @@ class Commitment(models.Model, CommitmentParent):
         COMPLETE = 1
         EXPIRED = 2
 
-    created = models.DateTimeField("Datetime of creation")
-    last_updated = models.DateTimeField("Datetime of last modification")
+    created = models.DateTimeField("Datetime of creation", auto_now_add=True)
+    last_updated = models.DateTimeField("Datetime of last modification", auto_now=True)
     title = models.CharField("Title", max_length=200)
     description = models.TextField("Detailed description", max_length=2000)
     status = models.IntegerField(choices=CommitmentStatus.choices)
