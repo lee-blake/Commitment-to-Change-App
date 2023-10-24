c075d20ab2d2efdc606fb410438af6f6611a2f3d
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-10-24 11:28:46 -0400

    Move demo views to their own file and path

--- Commitment_to_Change_App/commitments/urls.py
+++ Commitment_to_Change_App/commitments/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from . import views
+from . import views, demo_views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("template_demo", views.template_demo, name="template demo"),
-    path("model_details", views.test_models_show_details, name="model details"),
-    path("model_change_title", views.test_models_change_title, name="change title"),
-    path("model_change_description", views.test_models_change_description, name="change description"),
-    path("model_list_all", views.test_models_list_commitments, name="list all commitments"),
-    path("model_create", views.test_models_create_commitment, name="create commitment"),
-]
 
+    path("demo/template_demo", demo_views.template_demo, name="template demo"),
+    path("demo/model_details", demo_views.test_models_show_details, name="model details"),
+    path("demo/model_change_title", demo_views.test_models_change_title, name="change title"),
+    path("demo/model_change_description", demo_views.test_models_change_description, name="change description"),
+    path("demo/model_list_all", demo_views.test_models_list_commitments, name="list all commitments"),
+    path("demo/model_create", demo_views.test_models_create_commitment, name="create commitment"),
+]
