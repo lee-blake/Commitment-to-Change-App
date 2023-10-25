0fd42f5c2d98f59eab156f0506b1ff696547b8b0
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-10-25 10:51:57 -0400

    Remove demo routes around commitments that are mostly broken

--- Commitment_to_Change_App/commitments/urls.py
+++ Commitment_to_Change_App/commitments/urls.py
@@ -9,9 +9,4 @@ urlpatterns = [
     path("commitment/create", views.create_commitment, name="create commitment"),
 
     path("demo/template_demo", demo_views.template_demo, name="template demo"),
-    path("demo/model_details", demo_views.test_models_show_details, name="model details"),
-    path("demo/model_change_title", demo_views.test_models_change_title, name="change title"),
-    path("demo/model_change_description", demo_views.test_models_change_description, name="change description"),
-    path("demo/model_list_all", demo_views.test_models_list_commitments, name="list all commitments"),
-    path("demo/model_create", demo_views.test_models_create_commitment, name="create commitment"),
 ]
