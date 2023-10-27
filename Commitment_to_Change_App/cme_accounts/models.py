37d9d66fac4317646225ebbf304d68057c3b2f57
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-10-27 13:29:58 -0400

    Change to require emails to be unique

--- Commitment_to_Change_App/cme_accounts/models.py
+++ Commitment_to_Change_App/cme_accounts/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    # TODO Create a validator to prevent the username from being an email and register here
+    email = models.EmailField(max_length=254, unique=True)
