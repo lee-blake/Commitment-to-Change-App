1dd9ad14fa8a9cf8e3d875606a1265d75b48c5eb
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-10-27 13:27:18 -0400

    Implement simple but insecure login

--- Commitment_to_Change_App/cme_accounts/urls.py
+++ Commitment_to_Change_App/cme_accounts/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
+    # This imports all the default Django auth urls. See
+    # https://docs.djangoproject.com/en/4.2/topics/auth/default/#module-django.contrib.auth.views
+    # They are NOT generally working - we need to write templates for them.
+    path("", include("django.contrib.auth.urls")),
+    # create currently uses GET to create users and is not suitable for production. It needs a form to be created
+    # before we can switch to POST.
+    path("create", views.create_user_target, name="create user target"),
     path("display_logged_in_user", views.display_logged_in_user, name="index"),
+    # do_login and do_logout are 'testing' login and logout views. They use GET and are not suitable for production
+    # but are currently useful for basic testing.
+    path("do_login", views.do_login, name="login"),
+    path("do_logout", views.do_logout, name="logout")
 ]
--- Commitment_to_Change_App/cme_accounts/views.py
+++ Commitment_to_Change_App/cme_accounts/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+
+from .models import User
 
 
 def display_logged_in_user(request):
@@ -7,3 +10,34 @@ def display_logged_in_user(request):
         return HttpResponse("Logged in as {}".format(request.user.username))
     else:
         return HttpResponse("Not logged in")
+
+
+def create_user_target(request):
+    username = request.GET.get("username")
+    password = request.GET.get("password")
+    email = request.GET.get("email")
+    User.objects.create_user(
+        username=username,
+        password=password,
+        email=email
+    )
+    return HttpResponse("User {} created.".format(username))
+
+
+def do_login(request):
+    username = request.GET.get("username")
+    password = request.GET.get("password")
+    user = authenticate(username=username, password=password)
+    if user:
+        login(request, user)
+        return HttpResponse("Login successful")
+    else:
+        return HttpResponse("Login failed")
+
+
+def do_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return HttpResponse("Logout successful")
+    else:
+        return HttpResponse("Nobody was logged in")
