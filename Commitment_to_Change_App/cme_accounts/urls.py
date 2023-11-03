162b776b045f8eafa02f4dee8c4bc62f40d0d016
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-11-02 20:21:44 -0400

    Remove old account registration

--- Commitment_to_Change_App/cme_accounts/urls.py
+++ Commitment_to_Change_App/cme_accounts/urls.py
@@ -10,7 +10,4 @@ urlpatterns = [
     # https://docs.djangoproject.com/en/4.2/topics/auth/default/#module-django.contrib.auth.views
     # They are NOT generally working - we need to write templates for them.
     path("", include("django.contrib.auth.urls")),
-    # create currently uses GET to create users and is not suitable for production. It needs a form to be created
-    # before we can switch to POST.
-    path("create", views.create_user_target, name="create user target"),
 ]
--- Commitment_to_Change_App/cme_accounts/views.py
+++ Commitment_to_Change_App/cme_accounts/views.py
@@ -1,7 +1,4 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.http import HttpResponse
-
-from .models import User
 
 
 class SignInView(LoginView):
@@ -10,15 +7,3 @@ class SignInView(LoginView):
 
 class SignOutView(LogoutView):
     template_name = "accounts/logged_out.html"
-
-
-def create_user_target(request):
-    username = request.GET.get("username")
-    password = request.GET.get("password")
-    email = request.GET.get("email")
-    User.objects.create_user(
-        username=username,
-        password=password,
-        email=email
-    )
-    return HttpResponse("User {} created.".format(username))
