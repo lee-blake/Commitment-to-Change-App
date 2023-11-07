f1b0a8719a7cc82d5c169aedd0ed79d204208399
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-11-07 12:22:39 -0500

    Make the root url redirect to login

--- Commitment_to_Change_App/Commitment_to_Change_App/urls.py
+++ Commitment_to_Change_App/Commitment_to_Change_App/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('accounts/', include("cme_accounts.urls")),
     path('app/', include("commitments.urls")),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url="/accounts/login/"))
 ]
