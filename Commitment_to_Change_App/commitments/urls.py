dd70e5b8e73a08370a2d16b887cb662d2211efec
North-Kaster <bckaster@bsu.edu>
2023-11-09 00:55:40 -0500

    Adjust code formatting

--- Commitment_to_Change_App/commitments/urls.py
+++ Commitment_to_Change_App/commitments/urls.py
@@ -7,27 +7,10 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("commitment/<int:commitment_id>/view/", views.view_commitment, name="view commitment"),
     path("commitment/<int:commitment_id>/share/", views.view_commitment, name="share commitment"),
-    path(
-        "commitment/<int:commitment_id>/complete/",
-        views.CompleteCommitmentView.as_view(),
-        name="complete commitment"
-    ),
-    path(
-        "commitment/<int:commitment_id>/discontinue/",
-        views.DiscontinueCommitmentView.as_view(),
-        name="discontinue commitment"
-    ),
-    path(
-        "commitment/<int:commitment_id>/reopen/",
-        views.ReopenCommitmentView.as_view(),
-        name="reopen commitment"
-    ),
-    path(
-        "commitment/make/", views.MakeCommitmentView.as_view(), name="make commitment"
-    ),
-    path("commitment/<int:commitment_id>/delete/", views.DeleteCommitmentView.as_view(), name="delete commitment"
-         ),
-    path(
-        "commitment/<int:commitment_id>/edit/", views.EditCommitmentView.as_view(), name="edit commitment"
-    ),
+    path("commitment/<int:commitment_id>/complete/", views.CompleteCommitmentView.as_view(), name="complete commitment"),
+    path("commitment/<int:commitment_id>/discontinue/", views.DiscontinueCommitmentView.as_view(), name="discontinue commitment"),
+    path("commitment/<int:commitment_id>/reopen/", views.ReopenCommitmentView.as_view(), name="reopen commitment"),
+    path("commitment/make/", views.MakeCommitmentView.as_view(), name="make commitment"),
+    path("commitment/<int:commitment_id>/delete/", views.DeleteCommitmentView.as_view(), name="delete commitment"),
+    path("commitment/<int:commitment_id>/edit/", views.EditCommitmentView.as_view(), name="edit commitment"),
 ]
