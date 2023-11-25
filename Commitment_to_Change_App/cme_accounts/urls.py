bf7557bf309f911efd638c2182dd554b8ff401ad
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-11-25 18:21:19 -0500

    Remove untested Django urls from cme_accounts

--- Commitment_to_Change_App/cme_accounts/urls.py
+++ Commitment_to_Change_App/cme_accounts/urls.py
@@ -20,8 +20,5 @@ urlpatterns = [
         views.ResetPasswordCompleteView.as_view(), 
         name="password reset complete"
     ),
-    # This imports all the default Django auth urls. See
-    # https://docs.djangoproject.com/en/4.2/topics/auth/default/#module-django.contrib.auth.views
-    # They are NOT generally working - we need to write templates for them.
     path("", include("django.contrib.auth.urls")),
 ]
