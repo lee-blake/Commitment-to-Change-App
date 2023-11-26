1ecc7f27769b7793afc2bca32a28163fa6d75fc5
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-11-26 13:26:09 -0500

    Remove unused form
    Since our user creation now occurs in the registration app, the form
    here is no longer needed.

--- Commitment_to_Change_App/cme_accounts/forms.py
+++ Commitment_to_Change_App/cme_accounts/forms.py
@@ -3,7 +3,4 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = UserCreationForm.Meta.fields + ("email",)
+
