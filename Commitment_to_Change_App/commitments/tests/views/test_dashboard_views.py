cc0ef9816c08cf4e3bf057cfca5933da6aeb2fd0
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-11-30 09:26:00 -0500

    Add tests for ClinicianDashboardView

--- Commitment_to_Change_App/commitments/tests/views/test_dashboard_views.py
+++ Commitment_to_Change_App/commitments/tests/views/test_dashboard_views.py
@@ -1,10 +1,148 @@
 """Tests for dashboard views."""
 
+import datetime
 import re
 
 import pytest
 
 from django.urls import reverse
+
+from commitments.models import Commitment, Course
+
+
+@pytest.mark.django_db
+class TestClinicianDashboardView:
+    """Tests for ClinicianDashboardView"""
+
+    class TestGet:
+        """Tests for ClinicianDashboardView.get"""
+
+        @pytest.fixture(name="commitments_owned_by_saved_clinician_profile")
+        def fixture_commitments_owned_by_saved_clinician_profile(self, saved_clinician_profile):
+            return (
+                Commitment.objects.create(
+                    owner=saved_clinician_profile,
+                    title="First owned commitment",
+                    description="First commitment to test the dashboard with",
+                    deadline=datetime.date.today(),
+                    status=Commitment.CommitmentStatus.IN_PROGRESS
+                ),
+                Commitment.objects.create(
+                    owner=saved_clinician_profile,
+                    title="Another dashboard commitment",
+                    description="Second commitment to test the dashboard with",
+                    deadline=datetime.date.today(),
+                    status=Commitment.CommitmentStatus.COMPLETE
+                ),
+                Commitment.objects.create(
+                    owner=saved_clinician_profile,
+                    title="Last chance commitment to break the dashboard",
+                    description="Third commitment to test the dashboard with",
+                    deadline=datetime.date.today(),
+                    status=Commitment.CommitmentStatus.DISCONTINUED
+                ),
+            )
+
+        @pytest.fixture(name="courses_enrolling_only_saved_clinician_profile")
+        def fixture_courses_enrolling_only_saved_clinician_profile(
+            self, saved_provider_profile, saved_clinician_profile, enrolled_course
+        ):
+            courses =  (
+                enrolled_course,
+                Course.objects.create(
+                    owner=saved_provider_profile,
+                    title="Second course for the dashboard",
+                    description="Only saved_clinician_profile is a student"
+                ),
+                Course.objects.create(
+                    owner=saved_provider_profile,
+                    title="This course should show in the dashboard too",
+                    description="Only saved_clinician_profile is a student and this is the last one"
+                ),
+            )
+            courses[1].students.add(saved_clinician_profile)
+            courses[2].students.add(saved_clinician_profile)
+            return courses
+
+        def test_rejects_provider_users_with_403(self, client, saved_provider_profile):
+            client.force_login(saved_provider_profile.user)
+            response = client.get(reverse("clinician dashboard"))
+            assert response.status_code == 403
+
+        def test_all_owned_commitments_show_links_in_page(
+            self, client, saved_clinician_profile, commitments_owned_by_saved_clinician_profile
+        ):
+            client.force_login(saved_clinician_profile.user)
+            html = client.get(reverse("clinician dashboard")).content.decode()
+            for commitment in commitments_owned_by_saved_clinician_profile:
+                commitment_view_url = reverse(
+                    "view commitment",
+                    kwargs={"commitment_id": commitment.id}
+                )
+                commitment_view_link = f"href=\"{commitment_view_url}\""
+                assert commitment_view_link in html
+
+        def test_unowned_commitments_do_not_show_in_page(
+            self, client, other_clinician_profile, commitments_owned_by_saved_clinician_profile
+        ):
+            client.force_login(other_clinician_profile.user)
+            html = client.get(reverse("clinician dashboard")).content.decode()
+            for commitment in commitments_owned_by_saved_clinician_profile:
+                commitment_view_url = reverse(
+                    "view commitment",
+                    kwargs={"commitment_id": commitment.id}
+                )
+                commitment_view_link = f"href=\"{commitment_view_url}\""
+                assert commitment_view_link not in html
+                assert commitment.title not in html
+
+        def test_make_commitment_url_linked_in_page(
+            self, client, saved_clinician_profile
+        ):
+            client.force_login(saved_clinician_profile.user)
+            html = client.get(reverse("clinician dashboard")).content.decode()
+            make_commitment_url = reverse("make commitment")
+            make_commitment_link = f"href=\"{make_commitment_url}\""
+            assert make_commitment_link in html
+
+        def test_all_enrolled_courses_show_links_in_page(
+            self, client, saved_clinician_profile, courses_enrolling_only_saved_clinician_profile
+        ):
+            client.force_login(saved_clinician_profile.user)
+            html = client.get(reverse("clinician dashboard")).content.decode()
+            for course in courses_enrolling_only_saved_clinician_profile:
+                course_view_url = reverse(
+                    "view course",
+                    kwargs={"course_id": course.id}
+                )
+                course_link = f"href=\"{course_view_url}\""
+                assert course_link in html
+
+        def test_unenrolled_courses_do_not_show_in_page(
+            self, client, other_clinician_profile, courses_enrolling_only_saved_clinician_profile
+        ):
+            client.force_login(other_clinician_profile.user)
+            html = client.get(reverse("clinician dashboard")).content.decode()
+            for course in courses_enrolling_only_saved_clinician_profile:
+                course_view_url = reverse(
+                    "view course",
+                    kwargs={"course_id": course.id}
+                )
+                course_link = f"href=\"{course_view_url}\""
+                assert course_link not in html
+                assert course.title not in html
+
+
+    class TestPost:
+        """Tests for ClinicianDashboardView.post"""
+
+        def test_post_rejected_with_405(self, client, saved_clinician_profile):
+            client.force_login(saved_clinician_profile.user)
+            response = client.post(
+                reverse("clinician dashboard"),
+                {}
+            )
+            assert response.status_code == 405
 
 
 @pytest.mark.django_db
