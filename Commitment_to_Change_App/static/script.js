e5b86d6b7f1eb1227409aeda9671b98d111477ee
North-Kaster <bckaster@bsu.edu>
2023-12-03 13:27:43 -0500

    Fix showPassword function in script.js

--- Commitment_to_Change_App/static/script.js
+++ Commitment_to_Change_App/static/script.js
@@ -24,21 +24,11 @@ function copyToClipboard (id) {
       });
     }
 }
-function showPassword (id_password, id_showPassword1, id_showPassword2) {
-    const passwordInput = document.getElementById(id_showPassword);
-    const passwordInput1 = document.getElementById(id_showPassword1);
-    const passwordInput2 = document.getElementById(id_showPassword2);
-
-    const showPasswordBox = document.getElementById('showPassword');
-    const isPasswordVisible  = showPasswordBox.checked;
-
-    if (isPasswordVisible) {
-        passwordInput.type = 'text';
-        passwordInput1.type = 'text';
-        passwordInput2.type = 'text';
-    } else {
-        passwordInput.type = 'text';
-        passwordInput1.type = 'password';
-        passwordInput2.type = 'password';
-    }
+function showPassword (id) {
+    var selected_password_field = document.getElementById(id);
+  if (selected_password_field.type === "password") {
+    selected_password_field.type = "text";
+  } else {
+    selected_password_field.type = "password";
+  }
 }
