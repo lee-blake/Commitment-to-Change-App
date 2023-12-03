f3b4346cfad1ff32196a23cc4676e98868301ee2
Kris Thurman <kristhurman12@gmail.com>
2023-12-03 02:53:04 -0500

    add copy to clipboard function & add show password

--- Commitment_to_Change_App/static/script.js
+++ Commitment_to_Change_App/static/script.js
@@ -1,2 +1,26 @@
-//Put your JavaScript function here and delete this comment after:
+function copyToClipBoard (id) {
+    const copyInput = document.getElementById('copyInput');
+    copyInput.select();
+    copyInput.setSelectionRange(0, 9999);
 
+    document.execCommand('copy');
+    copyInput.blur();
+}
+function showPassword (id_password, id_showPassword1, id_showPassword2) {
+    const passwordInput = document.getElementById(id_showPassword);
+    const passwordInput1 = document.getElementById(id_showPassword1);
+    const passwordInput2 = document.getElementById(id_showPassword2);
+
+    const showPasswordBox = document.getElementById('showPassword');
+    const isPasswordVisible  = showPasswordBox.checked;
+
+    if (isPasswordVisible) {
+        passwordInput.type = 'text';
+        passwordInput1.type = 'text';
+        passwordInput2.type = 'text';
+    } else {
+        passwordInput.type = 'text';
+        passwordInput1.type = 'password';
+        passwordInput2.type = 'password';
+    }
+}
