cf3888d0f28f1bf6894e68abc071a4c99630e55f
North-Kaster <bckaster@bsu.edu>
2023-12-03 13:11:59 -0500

    Fix copyToClipboard function with correct implementation in template

--- Commitment_to_Change_App/static/script.js
+++ Commitment_to_Change_App/static/script.js
@@ -1,10 +1,28 @@
-function copyToClipBoard (id) {
-    const copyInput = document.getElementById('copyInput');
-    copyInput.select();
-    copyInput.setSelectionRange(0, 9999);
+function copyToClipboard (id) {
+    var text_to_copy = document.getElementById(id).innerHTML;
 
-    document.execCommand('copy');
-    copyInput.blur();
+    if (!navigator.clipboard){ //Check to see if browser supports clipboard API
+
+        // If not, use execCommand, an outdated method of copying. 
+        // The clipboard API is still newer, and some browsers still
+        // do not fully support it, so leaving this here just in case.
+        
+        var r = document.createRange();
+        r.selectNode(document.getElementById(id));
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(r);
+        document.execCommand('copy');
+        window.getSelection().removeAllRanges();
+    } else {
+    navigator.clipboard.writeText(text_to_copy).then(
+        function(){
+            alert("Copied Successfully");
+        })
+      .catch(
+         function() {
+            alert("Copy Failed");
+      });
+    }
 }
 function showPassword (id_password, id_showPassword1, id_showPassword2) {
     const passwordInput = document.getElementById(id_showPassword);
