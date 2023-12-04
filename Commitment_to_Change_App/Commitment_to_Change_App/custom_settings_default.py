3f93b9dc5d64ecfd4ec70b6f912eb5622bc73b96
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-12-04 11:20:42 -0500

    Make `custom_settings.py` override `settings.py`
    Previously, only specific settings from `custom_settings.py` were
    imported and applied. This would require modifying both files any
    time a new settings variable had to change. This commit makes it so
    that any settings variable set in `custom_settings.py` is respected.
    This will lead to fewer future changes in replication instructions
    and cleaner file structure.

--- Commitment_to_Change_App/Commitment_to_Change_App/custom_settings_default.py
+++ Commitment_to_Change_App/Commitment_to_Change_App/custom_settings_default.py
@@ -1,15 +1,10 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+"""This file contains defaults for custom settings that are unique to your Django
+instance. If you want to override anything in settings.py specific to your instance
+only, you should do it by adding the respective variable in this file. You should
+not do so in settings.py because that file is committed to the repo."""
 
+# You must generate a secret key for your server. Run the following code
+# in your terminal to do so and then copy it between the quotes:
+# python -c "import secrets; print(secrets.token_urlsafe())"
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
-
-EMAIL_HOST = "cme-ctc-mailcapture"
-EMAIL_PORT = 25
