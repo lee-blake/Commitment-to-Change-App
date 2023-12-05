5df15d3d9d9e5123b87c0d4813762ae2cbe1e239
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-12-04 19:09:51 -0500

    Add custom_settings.py defaults for GitHub CI

--- Commitment_to_Change_App/Commitment_to_Change_App/custom_settings_sources/custom_settings_github.py
+++ Commitment_to_Change_App/Commitment_to_Change_App/custom_settings_sources/custom_settings_github.py
@@ -3,21 +3,10 @@ instance. If you want to override anything in settings.py specific to your insta
 only, you should do it by adding the respective variable in this file. You should
 not do so in settings.py because that file is committed to the repo.
 
-These defaults are for the Docker replication instructions."""
+These defaults are for the GitHub CI workflow."""
 
 # You must generate a secret key for your server. Run the following code
 # in your terminal to do so and then copy it between the quotes:
 # python -c "import secrets; print(secrets.token_urlsafe())"
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
