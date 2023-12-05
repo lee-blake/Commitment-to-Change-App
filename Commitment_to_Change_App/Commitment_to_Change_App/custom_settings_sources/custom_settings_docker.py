4bc4acb53c39bc4c264a7c97f1dd3e2ec06a4f40
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2023-12-04 20:30:44 -0500

    Improve other custom_settings.py sources

--- Commitment_to_Change_App/Commitment_to_Change_App/custom_settings_sources/custom_settings_docker.py
+++ Commitment_to_Change_App/Commitment_to_Change_App/custom_settings_sources/custom_settings_docker.py
@@ -5,7 +5,7 @@ not do so in settings.py because that file is committed to the repo.
 
 These defaults are for the Docker replication instructions."""
 
-# You must generate a secret key for your server. Run the following code
+# TODO You must generate a secret key for your server. Run the following code
 # in your terminal to do so and then copy it between the quotes:
 # python -c "import secrets; print(secrets.token_urlsafe())"
 # SECURITY WARNING: keep the secret key used in production secret!
