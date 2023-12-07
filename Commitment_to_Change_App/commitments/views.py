af1b1515644d5eb627f1835888de744dcb93a16b
North-Kaster <bckaster@bsu.edu>
2023-12-07 16:33:01 -0500

    Move edit_commitment_template.html to Commitment directory

--- Commitment_to_Change_App/commitments/views.py
+++ Commitment_to_Change_App/commitments/views.py
@@ -459,7 +459,7 @@ class DeleteCommitmentTemplateView(ProviderLoginRequiredMixin, View):
 
 class EditCommitmentTemplateView(ProviderLoginRequiredMixin, UpdateView):
     form_class = CommitmentTemplateForm
-    template_name = "commitments/edit_commitment_template.html"
+    template_name = "commitments//Commitment/edit_commitment_template.html"
     pk_url_kwarg = "commitment_template_id"
     context_object_name = "commitment_template"
 
