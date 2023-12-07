e9762b26e274b7db2769c2f5e7f89092a29f414e
North-Kaster <bckaster@bsu.edu>
2023-12-07 16:20:16 -0500

    Move course_change_suggested_commitments.html to Course directory

--- Commitment_to_Change_App/commitments/views.py
+++ Commitment_to_Change_App/commitments/views.py
@@ -359,7 +359,7 @@ class ViewCommitmentTemplateView(ProviderLoginRequiredMixin, DetailView):
 
 class CourseChangeSuggestedCommitmentsView(ProviderLoginRequiredMixin, UpdateView):
     form_class = CourseSelectSuggestedCommitmentsForm
-    template_name = "commitments/course_change_suggested_commitments.html"
+    template_name = "commitments/Course/course_change_suggested_commitments.html"
     pk_url_kwarg = "course_id"
 
     def get_queryset(self):
