a196cf9e69f51e79c9398545fa37c9abbfd08674
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2024-01-27 11:54:35 -0500

    Refactor download views to be siblings

--- Commitment_to_Change_App/commitments/generic_views.py
+++ Commitment_to_Change_App/commitments/generic_views.py
@@ -7,6 +7,29 @@ from django.views.generic.base import View
 
 
 class GeneratedTemporaryFileDownloadView(View, ABC):
+    filename = ""
+    as_attachment = True
+
+    def get(self, *args, **kwargs):
+        # We use NamedTemporaryFile over TemporaryFile to enable testing that the cleanup
+        # actually occurs.
+        temp_file = tempfile.NamedTemporaryFile(mode="w+b", delete=True)
+        self._write_content_to_file(temp_file)
+        # Reset the head so the written content can be read
+        print(temp_file)
+        temp_file.seek(0)
+        return FileResponse(
+            temp_file,
+            filename=self.filename,
+            as_attachment=self.as_attachment
+        )
+
+    @abstractmethod
+    def _write_content_to_file(self, temporary_file):
+        return NotImplemented
+
+
+class GeneratedTemporaryBinaryFileDownloadView(GeneratedTemporaryFileDownloadView):
     """A generic view for dynamically generating & downloading a small file. The file will 
     automatically be cleaned up when the download has completed as it leverages `tempfile`.
     This also has the advantage of keeping the file in memory on Linux systemd distros.
@@ -17,22 +40,8 @@ class GeneratedTemporaryFileDownloadView(View, ABC):
     This view only supports binary writes. For text writes, use
     GeneratedTemporaryTextFileDownloadView instead."""
 
-    filename = ""
-    as_attachment = True
-
-    def get(self, *args, **kwargs):
-        # We use NamedTemporaryFile over TemporaryFile to enable testing that the cleanup
-        # actually occurs.
-        temp_file = tempfile.NamedTemporaryFile(mode="w+b", delete=True)
-        self.write_bytes_to_file(temp_file)
-        # Reset the head so the written content can be read
-        print(temp_file)
-        temp_file.seek(0)
-        return FileResponse(
-            temp_file,
-            filename=self.filename,
-            as_attachment=self.as_attachment
-        )
+    def _write_content_to_file(self, temporary_file):
+        self.write_bytes_to_file(temporary_file)
 
     @abstractmethod
     def write_bytes_to_file(self, temporary_file):
@@ -48,9 +57,9 @@ class GeneratedTemporaryTextFileDownloadView(GeneratedTemporaryFileDownloadView)
     worker during execution.
 
     This view only supports text writes. For binary writes, use
-    GeneratedTemporaryFileDownloadView instead."""
+    GeneratedTemporaryBinaryFileDownloadView instead."""
 
-    def write_bytes_to_file(self, temporary_file):
+    def _write_content_to_file(self, temporary_file):
         write_text_wrapped_temporary_file = io.TextIOWrapper(temporary_file)
         try:
             self.write_text_to_file(write_text_wrapped_temporary_file)
--- Commitment_to_Change_App/commitments/tests/test_generic_views.py
+++ Commitment_to_Change_App/commitments/tests/test_generic_views.py
@@ -3,17 +3,16 @@ import pytest
 
 from django.test import RequestFactory
 
-from commitments.generic_views import GeneratedTemporaryFileDownloadView, \
-    GeneratedTemporaryTextFileDownloadView
+from commitments.generic_views import GeneratedTemporaryBinaryFileDownloadView, \
+    GeneratedTemporaryTextFileDownloadView, GeneratedTemporaryFileDownloadView
 
 
 @pytest.fixture(name="trivial_request")
 def fixture_trivial_request():
     return RequestFactory().get("/")
 
-
-class TestGeneratedTemporaryFileDownloadView:
-    """Tests for GeneratedTemporaryFileDownloadView"""
+class TestGeneratedTemporaryBinaryFileDownloadView:
+    """Tests for GeneratedTemporaryBinaryFileDownloadView"""
 
     class TestClassParameters:
         """Tests that class fields of GeneratedTemporaryFileDownloadView are correctly used"""
@@ -23,46 +22,37 @@ class TestGeneratedTemporaryFileDownloadView:
             class ChildClass(GeneratedTemporaryFileDownloadView):
                 filename = file_name
 
-                def write_bytes_to_file(self, temporary_file):
+                def _write_content_to_file(self, temporary_file):
                     pass
 
             request = ChildClass.as_view()(trivial_request)
             assert request.filename == file_name
 
-        def test_as_attachment_defaults_to_true(self, trivial_request):
+        def test_as_attachment_defaults_to_false(self, trivial_request):
             class ChildClass(GeneratedTemporaryFileDownloadView):
-                def write_bytes_to_file(self, temporary_file):
-                    pass
-
-            request = ChildClass.as_view()(trivial_request)
-            assert request.as_attachment is True
-
-        def test_as_attachment_respects_false(self, trivial_request):
-            class ChildClass(GeneratedTemporaryFileDownloadView):
-                as_attachment = False
-
-                def write_bytes_to_file(self, temporary_file):
+                def _write_content_to_file(self, temporary_file):
                     pass
 
             request = ChildClass.as_view()(trivial_request)
             assert request.as_attachment is False
 
+        def test_as_attachment_respects_true(self, trivial_request):
+            class ChildClass(GeneratedTemporaryFileDownloadView):
+                as_attachment = True
+
+                def _write_content_to_file(self, temporary_file):
+                    pass
+
+            request = ChildClass.as_view()(trivial_request)
+            assert request.as_attachment is True
+
 
     class TestGet:
         """Tests for GeneratedTemporaryFileDownloadView.get"""
 
-        @pytest.mark.parametrize("content", [b"x", b"y"])
-        def test_file_contains_correct_content(self, content, trivial_request):
-            class ChildClass(GeneratedTemporaryFileDownloadView):
-                def write_bytes_to_file(self, temporary_file):
-                    temporary_file.write(content)
-            response = ChildClass.as_view()(trivial_request)
-            response_content = b"".join(response.streaming_content)
-            assert response_content == content
-
         def test_file_gets_cleaned_up(self, trivial_request):
             class ChildClass(GeneratedTemporaryFileDownloadView):
-                def write_bytes_to_file(self, temporary_file):
+                def _write_content_to_file(self, temporary_file):
                     temporary_file.write(b"x")
             response = ChildClass.as_view()(trivial_request)
             underlying_file_name = response.file_to_stream.name
@@ -72,6 +62,22 @@ class TestGeneratedTemporaryFileDownloadView:
             b"".join(response.streaming_content)
             del response
             assert not os.path.exists(underlying_file_name)
+
+
+class TestGeneratedTemporaryBinaryFileDownloadView:
+    """Tests for GeneratedTemporaryBinaryFileDownloadView"""
+
+    class TestGet:
+        """Tests for GeneratedTemporaryBinaryFileDownloadView.get"""
+
+        @pytest.mark.parametrize("content", [b"x", b"y"])
+        def test_file_contains_correct_content(self, content, trivial_request):
+            class ChildClass(GeneratedTemporaryBinaryFileDownloadView):
+                def write_bytes_to_file(self, temporary_file):
+                    temporary_file.write(content)
+            response = ChildClass.as_view()(trivial_request)
+            response_content = b"".join(response.streaming_content)
+            assert response_content == content
 
 
 class TestGeneratedTemporaryTextFileDownloadView:
