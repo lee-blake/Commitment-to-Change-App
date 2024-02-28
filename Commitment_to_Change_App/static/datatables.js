8408f1be3c23a3d83e79f4088bd1f57e6b1e32c7
North-Kaster <bckaster@bsu.edu>
2024-02-28 12:41:01 -0500

    Extract table selector id out of datatable creation

--- Commitment_to_Change_App/static/datatables.js
+++ Commitment_to_Change_App/static/datatables.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-    $('#myTable').DataTable( {
-        "autoWidth": false,
-        "paging": false,
-        "bInfo" : false,
-        "columnDefs": [
-        {"className": "dt-center", "targets": "_all"}
-        ],
-      });
+    createStandardDataTable('#provider-course-datatable');
+    createStandardDataTable('#provider-commitment-template-datatable');
 });
+
+function createStandardDataTable(table_id){
+    $(table_id).DataTable({
+        // autoWidth scales onlyon page refresh. Disabling allows us to use bootstrap class scaling
+        "autoWidth": false,
+        // Disable pagination; Always displays everything in the list
+        "paging": false,
+        // Remove pagination information ("Showing 1 to N of N entries")
+        "bInfo" : false,
+        // Wraps the search bar with a "text-center" div
+        "dom": '<"text-center"f>i',
+        // Adds dt-center (dataTables class) to all columns to center column content
+        "columnDefs": [
+            {"className": "dt-center", "targets": "_all"}
+        ],
+    });
+}
