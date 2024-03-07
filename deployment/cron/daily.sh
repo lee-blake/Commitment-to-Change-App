680cf00063ada1c3e3ea25cd63a7d1d2ef29b907
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2024-03-07 15:49:10 -0500

    Add send_reminder_emails to Docker and cron

--- deployment/cron/daily.sh
+++ deployment/cron/daily.sh
@@ -2,3 +2,4 @@
 # Tasks that cron will run daily should be included here. Comment them out in
 # deployment if you wish to disable them.
 `dirname $0`/expire_commitments.sh
+`dirname $0`/send_reminder_emails.sh
