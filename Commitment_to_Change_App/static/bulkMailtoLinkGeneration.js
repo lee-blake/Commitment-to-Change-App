1499701548618cbcd6e895e9c1fc359f17a8c7fa
North-Kaster <bckaster@bsu.edu>
2024-03-11 11:56:53 -0400

    Refactor bulk email js

--- Commitment_to_Change_App/static/bulkMailtoLinkGeneration.js
+++ Commitment_to_Change_App/static/bulkMailtoLinkGeneration.js
@@ -1,11 +1,16 @@
-$(document).ready(function () {
-  $('#modal-bulk-email-submit-button').click(function () {
-    const selectedEmails = [];
-    $('.email-checkbox:checked').each(function () {
-      selectedEmails.push($(this).data('email'));
-    });
-
-    const mailtoLink = 'mailto:' + selectedEmails.join(',');
-    window.location.href = mailtoLink;
-  });
+$(document).ready(function() {
+  $('#modal-bulk-email-submit-button').click(generateMailtoLink);
 });
+
+function generateMailtoLink() {
+  const selectedEmails = [];
+  $('.email-checkbox:checked').each(function() {
+    selectedEmails.push($(this).data('email'));
+  });
+  const mailtoLink = 'mailto:' + selectedEmails.join(',');
+  redirectToMailtoLink(mailtoLink);
+}
+
+function redirectToMailtoLink(mailtoLink) {
+  window.location.href = mailtoLink;
+}
