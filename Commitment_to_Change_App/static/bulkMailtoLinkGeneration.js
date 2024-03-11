b6b00d0119006578f7c65444da86433d9be9f96e
North-Kaster <bckaster@bsu.edu>
2024-03-11 11:57:16 -0400

    Add select all checkbox

--- Commitment_to_Change_App/static/bulkMailtoLinkGeneration.js
+++ Commitment_to_Change_App/static/bulkMailtoLinkGeneration.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
+  $('#select-all-checkbox').click(selectAllCheckboxes);
   $('#modal-bulk-email-submit-button').click(generateMailtoLink);
 });
 
+function selectAllCheckboxes() {
+  $('.email-checkbox').prop('checked', this.checked);
+} 
+
 function generateMailtoLink() {
+  const selectedEmails = getSelectedEmails();
+  const mailtoLink = 'mailto:' + selectedEmails.join(',');
+  redirectToMailtoLink(mailtoLink);
+}
+
+function getSelectedEmails() {
   const selectedEmails = [];
   $('.email-checkbox:checked').each(function() {
     selectedEmails.push($(this).data('email'));
   });
-  const mailtoLink = 'mailto:' + selectedEmails.join(',');
-  redirectToMailtoLink(mailtoLink);
+  return selectedEmails;
 }
 
 function redirectToMailtoLink(mailtoLink) {
