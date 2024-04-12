ed5a7efe7f3085a6e29963f18dd186f16c9a96a5
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2024-04-12 10:34:54 -0400

    Reduce duplication

--- Commitment_to_Change_App/commitments/management/commands/send_reminder_emails.py
+++ Commitment_to_Change_App/commitments/management/commands/send_reminder_emails.py
@@ -11,18 +11,17 @@ def send_one_time_reminder_emails_for_commitments():
         # Include prior days in case email failed to send previously.
         date__lte=datetime.date.today(),
     ).all()
-    for email in emails_scheduled_for_today_or_earlier:
-        try:
-            email.send()
-        except SMTPException:
-            pass # Do not cause the whole batch to fail if only one is a problem.
+    try_to_send_all_emails(emails_scheduled_for_today_or_earlier)
 
 def send_recurring_reminder_emails_for_commitments():
     recurring_emails_scheduled_for_today_or_earlier = RecurringReminderEmail.objects.filter(
         # Include prior days in case email failed to send previously.
         next_email_date__lte=datetime.date.today()
     ).all()
-    for email in recurring_emails_scheduled_for_today_or_earlier:
+    try_to_send_all_emails(recurring_emails_scheduled_for_today_or_earlier)
+
+def try_to_send_all_emails(emails):
+    for email in emails:
         try:
             email.send()
         except SMTPException:
