37901430b5c42c0fb8edaef0266522d8ae9c0896
North-Kaster <bckaster@bsu.edu>
2024-04-15 01:44:38 -0400

    Add theme icon to toggle button

--- Commitment_to_Change_App/static/scripts/websiteThemeToggle.js
+++ Commitment_to_Change_App/static/scripts/websiteThemeToggle.js
@@ -1,7 +1,17 @@
 function setUserPreferredTheme(userPreferTheme) {
     document.documentElement.setAttribute('data-theme', userPreferTheme);
     // Save theme selection in local storage
-    localStorage.setItem('theme', userPreferTheme); 
+    localStorage.setItem('theme', userPreferTheme);
+    setThemeIcon(userPreferTheme);
+}
+
+function setThemeIcon(userPreferTheme) {
+var themeIcon = document.getElementById('theme-icon');
+    if (userPreferTheme === 'dark') {
+        themeIcon.className = 'bi bi-moon-stars-fill';
+    } else {
+        themeIcon.className = 'bi bi-sun-fill';
+    }
 }
 
 function toggleTheme() {
